93929cf674f5d78d1f0d516faaec849b06490efd
Hitesh709 <hitesh709@github.com>
2026-08-03 18:27:23 +0530

    Add explicit webpack @ alias so builds resolve @/* without TypeScript dev deps

--- web/next.config.js
+++ web/next.config.js
@@ -1,8 +1,17 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
   images: {
     domains: [],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.join(__dirname),
+    }
+    return config
   },
 }
 
