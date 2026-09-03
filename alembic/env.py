47915a1a7d9205837e849f5789410bd87f05326b
Hitesh709 <Hiteshkansara4@gmail.com>
2026-09-04 03:39:20 +0530

    chore: remove unused migration import

--- alembic/env.py
+++ alembic/env.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 
 import asyncio
 from logging.config import fileConfig
-from urllib.parse import urlsplit
 
 from alembic import context
 from sqlalchemy import pool
